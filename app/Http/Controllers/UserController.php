7e77e5326a8747c117b371fa44399fe90b100f01
JuanTrop <jortiza2@unicartagena.edu.co>
2020-12-17 19:37:54 -0500

    Practica curso platzi

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::latest()->get();
+
+        return view('users.index', [
+            'users' => $user
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8']
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+                  ]);
+
+                  return back();
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return back();    
+    }
+}
